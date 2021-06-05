942bfa38cbdb82fb49fe4eb21ec8ed4e3596e5c6
vaibhavhooda <vaibhav.hooda1@gmail.com>
2021-06-05 16:21:25 +0530

    Done some code improvements in html/css

--- application/routes.py
+++ application/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
