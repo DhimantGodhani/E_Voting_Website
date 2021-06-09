9735ab9a4f598b17d401776f3a4b3f4c94df4ddf
aayushrishi <aayushrishi09@gmail.com>
2021-06-09 16:24:50 +0530

    added html/css page for futureEvents, past_results, voteNow code changes in route,home_styling, index

--- application/routes.py
+++ application/routes.py
@@ -15,3 +15,19 @@ def register():
 @app.route("/login")
 def login():
     return render_template("login.html", login=True)
+
+@app.route("/past_results")
+def past_results():
+    partyData = [{"year":"2017", "partyID":"1111", "partyName":"SOLC", "partyVotes":"400", "result":"Winner"},
+{"year":"2018", "partyID":"2222", "partyName":"LGS", "partyVotes":"450", "result":"Winner"},
+{"year":"2019", "partyID":"3333", "partyName":"SOPU", "partyVotes":"500", "result":"Winner"},
+{"year":"2020", "partyID":"4444", "partyName":"SYFI", "partyVotes":"380", "result":"Winner"}]
+    return render_template("past_results.html", login=True, partyData = partyData)
+
+@app.route("/voteNow")
+def voteNow():
+    return render_template("voteNow.html", login=True)
+
+@app.route("/futureEvents")
+def futureEvents():
+    return render_template("futureEvents.html", login=True)
