fff3c5afd632aaee3fb24158dd205eda1100c702
DHIMANT <dhimantgm95@gmail.com>
2021-07-15 00:49:38 -0400

    Added register functionality with db connection

--- application/db_connection.py
+++ application/db_connection.py
@@ -5,5 +5,5 @@ class db_connection:
     def connect_to_mongodb(self):
         connection_string = "mongodb+srv://dbUser:admin@cluster0.3zkhv.mongodb.net/db_college?retryWrites=true&w=majority "
         my_client = pymongo.MongoClient(connection_string)
-        db = my_client["db_college"]
+        db = my_client["db_e_voting"]
         return db
